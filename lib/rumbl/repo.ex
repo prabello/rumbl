7add8ef426ba6a6870f706566867adcc1dce2e83
Pedro Rabello <pedro.rabello@mercadolivre.com>
2018-12-04 13:48:58 -0200

    Not running

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -8,7 +8,9 @@ defmodule Rumbl.Repo do
   def all(Rumbl.User) do
     [%Rumbl.User{id: "1", name: "José", username: "josevalim", password: "elixir"},
     %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-    %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}] end
+    %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}] 
+  end
+
   def all(_module), do: []
 
   def get(module, id) do
