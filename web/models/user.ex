bc8cf58d0493974b42ea3bd947a8a034905d306b
Pedro Rabello <pedro.rabello@mercadolivre.com>
2018-12-17 08:19:01 -0200

    user change

--- web/models/user.ex
+++ web/models/user.ex
@@ -9,6 +9,14 @@ defmodule Rumbl.User do
     |> validate_length(:username, min: 1, max: 20)
   end
 
+  def registrator_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min:6, max: 100)
+    |> put_pass_hash()
+  end
+
   schema "users" do
     field(:name, :string)
     field(:username, :string)
